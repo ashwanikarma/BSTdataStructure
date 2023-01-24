c102673da8011b8dfafc682cd813de672d0badbc
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-24 23:03:32 +0530

    [Ashwani] - created BST

---
+++ BinarySearchTreeAssign/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySearchTreeAssign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Practice problems!!!");            
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);            
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Display();                                            
+        }
+    }
+    
+}
